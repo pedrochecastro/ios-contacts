189faaf8263f5bf12d4d185169d982f5734fcf04
Pedro Sánchez <pedro.sanchez.it@gmail.com>
2020-01-23 17:59:09 +0100

    Edit Contact. Simple 1:1 cropped Image

--- Contacts/ViewControllers/ContactEditionViewController.swift
+++ Contacts/ViewControllers/ContactEditionViewController.swift
@@ -204,6 +204,40 @@ class ContactEditionViewController: UITableViewController {
     }
     
   }
+  
+  func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+    
+    let cgimage = image.cgImage!
+    let contextImage: UIImage = UIImage(cgImage: cgimage)
+    let contextSize: CGSize = contextImage.size
+    var posX: CGFloat = 0.0
+    var posY: CGFloat = 0.0
+    var cgwidth: CGFloat = CGFloat(width)
+    var cgheight: CGFloat = CGFloat(height)
+    
+    // See what size is longer and create the center off of that
+    if contextSize.width > contextSize.height {
+      posX = ((contextSize.width - contextSize.height) / 2)
+      posY = 0
+      cgwidth = contextSize.height
+      cgheight = contextSize.height
+    } else {
+      posX = 0
+      posY = ((contextSize.height - contextSize.width) / 2)
+      cgwidth = contextSize.width
+      cgheight = contextSize.width
+    }
+    
+    let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+    
+    // Create bitmap image from context using the rect
+    let imageRef: CGImage = cgimage.cropping(to: rect)!
+    
+    // Create a new image based on the imageRef and rotate back to the original orientation
+    let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+    
+    return image
+  }
 
     
 }
@@ -232,13 +266,15 @@ extension ContactEditionViewController: UIImagePickerControllerDelegate, UINavig
   
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     // Handle image
-    
+    let croppedImage: UIImage?
     if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-      contact?.contactImage = image
-      contactImage.image = contact?.contactImage ?? image
+      croppedImage = cropToBounds(image: image, width: 20.0, height: 20.0)
+      contact?.contactImage = croppedImage
+      contactImage.image = contact?.contactImage ?? croppedImage
     } else{
       print("Something went wrong in  image")
     }
+    
     if let _ = contact {
       doneBarButton.isEnabled = true
     }
